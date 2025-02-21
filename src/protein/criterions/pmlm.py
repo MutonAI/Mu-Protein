5ee702762f195fd9e1bfe317a886d7b7b72a3e75
MutonAI <soulsight.cn@gmail.com>
2025-02-21 20:05:44 +0800

    add pmlm and mu-search, update README files

--- src/protein/criterions/pmlm.py
+++
@@ -1,6 +0,0 @@
-from fairseq.criterions import FairseqCriterion, register_criterion
-
-@register_criterion('prot_pmlm')
-class ProtPairwiseMaskedLMCriterion(FairseqCriterion):
-    def __init__(self, task, tpu=False):
-        super().__init__(task)
